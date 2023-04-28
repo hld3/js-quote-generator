12a330734d534cc672f760fb09965c1285e6808b
Harry Dodson <harry.lee.dodson@gmail.com>
2023-04-28 12:38:46 -0400

    added loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let quotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Populate quotes to the page
 function newQuote() {
+  loading();
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
   authorText.textContent = quote.author ?? 'Unknown';
@@ -16,11 +30,15 @@ function newQuote() {
   quote.text.length > 120
     ? quoteText.classList.add('long-quote')
     : quoteText.classList.remove('long-quote');
+
+  // Hide loader
+  complete();
 }
 
 // Get quotes from the API
 async function retrieveQuotes() {
   try {
+    loading();
     const response = await fetch('https://type.fit/api/quotes');
     quotes = await response.json();
     newQuote();
